cda0f3dfb695ac45e3f1a3b065456f5c8c72325b
Magnus Ahlstromer V <mahlstromerv@gmail.com>
2023-04-01 12:01:24 -0700

    Update for cross platform

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-vendor/bin/premake5.exe gmake
+vendor/bin/"$1"/premake5 gmake
 
-if [ "$1" == "debug" ]; then
+if [ "$2" == "debug" ]; then
     echo "Performing debug build..."
     make all config=debug
     cd bin/Debug-windows/RelicApp
     ./RelicApp.exe
-elif [ "$1" == "release" ]; then
+elif [ "$2" == "release" ]; then
     echo "Performing release build..."
     make all config=release
     cd bin/Release-windows/RelicApp
     ./RelicApp.exe    
-elif [ "$1" == "dist" ]; then
+elif [ "$2" == "dist" ]; then
     echo "Performing distribution release..."
     make all config=dist
     cd bin/Dist-windows/RelicApp
