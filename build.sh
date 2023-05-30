5b99a3d39ff11746d5bbfea170c0ca1bf9518420
Magnus Ahlstromer V <mahlstromerv@gmail.com>
2023-05-30 11:37:11 -0700

    Allow 7 threads to be used when compiling

--- build.sh
+++ build.sh
@@ -8,13 +8,14 @@ fi
 
 if [ "$2" == "debug" ]; then
     echo "Performing debug build..."
-    make all config=debug
+    make all config=debug -j7
 elif [ "$2" == "release" ]; then
     echo "Performing release build..."
-    make all config=release
+    make all config=release -j7
 elif [ "$2" == "dist" ]; then
     echo "Performing distribution release..."
-    make all config=dist
+    make all config=dist -j7
 else
     echo "Invalid argument"
 fi
+
